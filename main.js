793f43de128bd1d9031e830a7bb5f74a8be3288a
nathasjafink <nathasja.nielsen@gmail.com>
2023-10-29 12:21:52 +0100

    comments removed

--- main.js
+++ main.js
@@ -1,18 +1,3 @@
-/*
-Minut to second = minut * 60
-Hour to seconds = hour * 3600
-const seconds = minutes * 60;
-    return seconds;
- */
-/*
-const btn1 = document.querySelector('#btn1')
-function minuteToSeconds () {
-    const minutes = document.querySelector('#input');
-    const seconds = document.querySelector('#output');
-    seconds.innerText = "Total Seconds: " + minutes.value * 60;
-}
-btn1.addEventListener('click',minuteToSeconds);
-*/
 const btn3 = document.querySelector('#btn3');
 function dropDownCalculator () {
     const selected = document.querySelector('#select-calculator');
